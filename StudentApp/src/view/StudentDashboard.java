0c762a8e2abcaebaaf866fb9c1d54acf24e34d70
Aprajita Thakur <athaku20@asu.edu>
2019-04-03 22:13:09 -0700

    Added Dashboard in MVC Structure

---
+++ StudentApp/src/view/StudentDashboard.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class StudentDashboard  {
+	JFrame frame = new JFrame("Quiz Application");
+	JPanel panel = new JPanel();
+	String quizzes[] = { "Quiz1", "Quiz2", "Quiz3", "Quiz4", "Quiz5" };
+	JComboBox comboBox= new JComboBox(quizzes);
+	JButton button = new JButton("Take Quiz");
+	JLabel pageName = new JLabel("Student Dashboard");
+
+	JLabel selectQuizName = new JLabel("Select Quiz"); 
+
+
+	public StudentDashboard() {
+		panel.setLayout(new GridLayout(4,1,2,30));
+		panel.add(pageName);
+		panel.add(selectQuizName);
+		panel.add(comboBox);
+		panel.add(button);
+		
+		frame.add(panel);
+		
+		frame.pack();
+		frame.setSize(500,500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
---
+++ StudentApp/src/view/StudentView.java
@@ -0,0 +1,11 @@
+package view;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class StudentView {
+
+	public static void main(String[] args) {
+		new StudentDashBoard();
+	}
+
+}
